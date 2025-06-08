d0f20fd7795a209a22f213ec8afe01abde37c63b
AKRISHNASRIKAR <akrishnasrikar@gmail.com>
2025-06-08 22:57:57 +0530

    Responsiveness fix

--- src/components/common/PopularMoviesSection.jsx
+++ src/components/common/PopularMoviesSection.jsx
@@ -8,7 +8,7 @@ const PopularMoviesSection = ({ movies }) => {
         What's Popular
       </h2>
 
-      <div className="flex flex-nowrap lg:overflow-hidden md:overflow-hidden sm:overflow-x-auto py-2 gap-3 px-4 md:gap-4 md:px-10">
+      <div className="flex flex-nowrap scrollbar-hide overflow-x-auto py-2 gap-3 px-4 md:gap-4 md:px-10 ">
         {movies.map((movie) => (
           <div
             key={movie.id}
